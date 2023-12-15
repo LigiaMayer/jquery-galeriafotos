9df699dcc0d7ffa8f121b1bc99c1d640b6ca3de2
Ligia mayer rodrigues <ligia-angela@hotmail.com>
2023-12-15 18:52:50 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+        $('header button').ready.click(function(){
+            $('form').slidedown();
+        })
+        $(#botao-cancelar).click(function(){
+            $('form').slideUp();
+        })
+
+        $('form').on('submit',function(e){
+            e.preventDefault();
+            const enderecoDaNovaImagem -$('#endereco-imagem-nova').val();
+            const novoItem = $('<li style="display: none"></li>');
+            $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+            $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(5000);
+        $('#endereco-imagem-nova').val(''); 
+        })
+
+
+})
